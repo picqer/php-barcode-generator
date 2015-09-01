51bb14bfee8ca7a8e758fa8d57a8e4852a10cb0e
Casper Bakker <casper@casperbakker.com>
2015-09-01 14:00:59 +0200

    Added license information

--- src/BarcodeGenerator.php
+++ src/BarcodeGenerator.php
@@ -7,6 +7,26 @@
  * Based on TCPDF Barcode Generator
  */
 
+// Copyright (C) 2002-2015 Nicola Asuni - Tecnick.com LTD
+//
+// This file is part of TCPDF software library.
+//
+// TCPDF is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// TCPDF is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the License
+// along with TCPDF. If not, see
+// <http://www.tecnick.com/pagefiles/tcpdf/LICENSE.TXT>.
+//
+// See LICENSE.TXT file for more information.
+
 namespace Picqer\Barcode;
 
 abstract class BarcodeGenerator
