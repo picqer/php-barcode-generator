04401df55a4def2fc347fd669481abfe1aa3b94b
Casper Bakker <casper@picqer.com>
2019-12-31 21:36:43 +0100

    Update to phpunit 8.5

--- tests/BarcodeTest.php
+++ tests/BarcodeTest.php
@@ -1,11 +1,10 @@
 <?php
 
-class BarcodeTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class BarcodeTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function png_barcode_generator_can_generate_code_128_barcode()
+    public function test_png_barcode_generator_can_generate_code_128_barcode()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
         $generated = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
@@ -13,10 +12,7 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('PNG', substr($generated, 1, 3));
     }
 
-    /**
-     * @test
-     */
-    public function svg_barcode_generator_can_generate_ean_13_barcode()
+    public function test_svg_barcode_generator_can_generate_ean_13_barcode()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generated = $generator->getBarcode('081231723897', $generator::TYPE_EAN_13);
@@ -24,10 +20,7 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile('tests/verified-files/081231723897-ean13.svg', $generated);
     }
 
-    /**
-     * @test
-     */
-    public function html_barcode_generator_can_generate_code_128_barcode()
+    public function test_html_barcode_generator_can_generate_code_128_barcode()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
         $generated = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
@@ -35,29 +28,23 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile('tests/verified-files/081231723897-code128.html', $generated);
     }
 
-    /**
-     * @test
-     */
-    public function jpg_barcode_generator_can_generate_code_128_barcode()
+    public function test_jpg_barcode_generator_can_generate_code_128_barcode()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorJPG();
-        $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
+
+        $this->assertGreaterThan(1000, strlen($result));
     }
 
-    /**
-     * @test
-     * @expectedException \Picqer\Barcode\Exceptions\InvalidCharacterException
-     */
-    public function ean13_generator_throws_exception_if_invalid_chars_are_used()
+    public function test_ean13_generator_throws_exception_if_invalid_chars_are_used()
     {
+        $this->expectException(Picqer\Barcode\Exceptions\InvalidCharacterException::class);
+
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generator->getBarcode('A123', $generator::TYPE_EAN_13);
     }
 
-    /**
-     * @test
-     */
-    public function ean13_generator_accepting_13_chars()
+    public function test_ean13_generator_accepting_13_chars()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generated = $generator->getBarcode('0049000004632', $generator::TYPE_EAN_13);
@@ -65,10 +52,7 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile('tests/verified-files/0049000004632-ean13.svg', $generated);
     }
 
-    /**
-     * @test
-     */
-    public function ean13_generator_accepting_12_chars_and_generates_13th_check_digit()
+    public function test_ean13_generator_accepting_12_chars_and_generates_13th_check_digit()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generated = $generator->getBarcode('004900000463', $generator::TYPE_EAN_13);
@@ -76,10 +60,7 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile('tests/verified-files/0049000004632-ean13.svg', $generated);
     }
 
-    /**
-     * @test
-     */
-    public function ean13_generator_accepting_11_chars_and_generates_13th_check_digit_and_adds_leading_zero()
+    public function test_ean13_generator_accepting_11_chars_and_generates_13th_check_digit_and_adds_leading_zero()
     {
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generated = $generator->getBarcode('04900000463', $generator::TYPE_EAN_13);
@@ -87,22 +68,18 @@ class BarcodeTest extends PHPUnit_Framework_TestCase
         $this->assertStringEqualsFile('tests/verified-files/0049000004632-ean13.svg', $generated);
     }
 
-    /**
-     * @test
-     * @expectedException \Picqer\Barcode\Exceptions\InvalidCheckDigitException
-     */
-    public function ean13_generator_throws_exception_when_wrong_check_digit_is_given()
+    public function test_ean13_generator_throws_exception_when_wrong_check_digit_is_given()
     {
+        $this->expectException(Picqer\Barcode\Exceptions\InvalidCheckDigitException::class);
+
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generator->getBarcode('0049000004633', $generator::TYPE_EAN_13);
     }
 
-    /**
-     * @test
-     * @expectedException \Picqer\Barcode\Exceptions\UnknownTypeException
-     */
-    public function generator_throws_unknown_type_exceptions()
+    public function test_generator_throws_unknown_type_exceptions()
     {
+        $this->expectException(Picqer\Barcode\Exceptions\UnknownTypeException::class);
+
         $generator = new Picqer\Barcode\BarcodeGeneratorSVG();
         $generator->getBarcode('0049000004633', 'vladimir');
     }
