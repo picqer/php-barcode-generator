253d74a52cd563ba5b91801594a6c1be569bcaa1
Casper Bakker <casper@picqer.com>
2020-01-02 21:30:48 +0100

    Merge PNG and JPG generators

--- src/BarcodeGeneratorJPG.php
+++ src/BarcodeGeneratorJPG.php
@@ -3,77 +3,20 @@
 namespace Picqer\Barcode;
 
 use Imagick;
-use imagickdraw;
-use imagickpixel;
-use Picqer\Barcode\Exceptions\BarcodeException;
 
-class BarcodeGeneratorJPG extends BarcodeGenerator
+class BarcodeGeneratorJPG extends BarcodeGeneratorPNG
 {
-    /**
-     * Return a JPG image representation of barcode (requires GD or Imagick library).
-     *
-     * @param string $barcode code to print
-     * @param string $type type of barcode:
-     * @param int $widthFactor Width of a single bar element in pixels.
-     * @param int $totalHeight Height of a single bar element in pixels.
-     * @param array $foregroundColor RGB (0-255) foreground color for bar elements (background is transparent).
-     * @return string image data or false in case of error.
-     * @public
-     * @throws BarcodeException
-     */
-    public function getBarcode($barcode, $type, int $widthFactor = 2, int $totalHeight = 30, array $foregroundColor = [0, 0, 0])
+    protected function createImagickImageObject(int $width, int $height): Imagick
     {
-        $barcodeData = $this->getBarcodeData($barcode, $type);
-
-        // calculate image size
-        $width = ($barcodeData['maxWidth'] * $widthFactor);
-        $height = $totalHeight;
-
-        if (function_exists('imagecreate')) {
-            // GD library
-            $imagick = false;
-            $jpg = imagecreate($width, $height);
-            $colorBackground = imagecolorallocate($jpg, 255, 255, 255);
-            imagecolortransparent($jpg, $colorBackground);
-            $colorForeground = imagecolorallocate($jpg, $foregroundColor[0], $foregroundColor[1], $foregroundColor[2]);
-        } elseif (extension_loaded('imagick')) {
-            $imagick = true;
-            $colorForeground = new imagickpixel('rgb(' . $foregroundColor[0] . ',' . $foregroundColor[1] . ',' . $foregroundColor[2] . ')');
-            $jpg = new Imagick();
-            $jpg->newImage($width, $height, 'white', 'jpg');
-            $imageMagickObject = new imagickdraw();
-            $imageMagickObject->setFillColor($colorForeground);
-        } else {
-            throw new BarcodeException('Neither gd-lib or imagick are installed!');
-        }
-
-        // print bars
-        $positionHorizontal = 0;
-        foreach ($barcodeData['bars'] as $bar) {
-            $bw = round(($bar['width'] * $widthFactor), 3);
-            $bh = round(($bar['height'] * $totalHeight / $barcodeData['maxHeight']), 3);
-            if ($bar['drawBar']) {
-                $y = round(($bar['positionVertical'] * $totalHeight / $barcodeData['maxHeight']), 3);
-                // draw a vertical bar
-                if ($imagick && isset($imageMagickObject)) {
-                    $imageMagickObject->rectangle($positionHorizontal, $y, ($positionHorizontal + $bw), ($y + $bh));
-                } else {
-                    imagefilledrectangle($jpg, $positionHorizontal, $y, ($positionHorizontal + $bw) - 1, ($y + $bh),
-                        $colorForeground);
-                }
-            }
-            $positionHorizontal += $bw;
-        }
-        ob_start();
-        if ($imagick && isset($imageMagickObject)) {
-            $jpg->drawImage($imageMagickObject);
-            echo $jpg;
-        } else {
-            imagejpeg($jpg);
-            imagedestroy($jpg);
-        }
-        $image = ob_get_clean();
+        $image = new Imagick();
+        $image->newImage($width, $height, 'none', 'JPG');
 
         return $image;
+    }
+
+    protected function generateGdImage($image)
+    {
+        imagejpeg($image);
+        imagedestroy($image);
     }
 }
