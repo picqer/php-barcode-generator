ff3a19a74017dcbba19ca87e5bf95f602e8165b6
Casper Bakker <casper@picqer.com>
2020-01-02 21:05:53 +0100

    Test different jpg/png image sizes

--- tests/BarcodeTest.php
+++ tests/BarcodeTest.php
@@ -33,7 +33,88 @@ class BarcodeTest extends TestCase
         $generator = new Picqer\Barcode\BarcodeGeneratorJPG();
         $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128);
 
+        $imageInfo = getimagesizefromstring($result);
+
         $this->assertGreaterThan(1000, strlen($result));
+        $this->assertEquals(202, $imageInfo[0]); // Image width
+        $this->assertEquals(30, $imageInfo[1]); // Image height
+        $this->assertEquals('image/jpeg', $imageInfo['mime']);
+    }
+
+    public function test_jpg_barcode_generator_can_generate_code_39_barcode()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorJPG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_39, 1);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(1000, strlen($result));
+        $this->assertEquals(224, $imageInfo[0]); // Image width
+        $this->assertEquals(30, $imageInfo[1]); // Image height
+        $this->assertEquals('image/jpeg', $imageInfo['mime']);
+    }
+
+    public function test_jpg_barcode_generator_can_use_different_height()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorJPG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128, 2, 45);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(1000, strlen($result));
+        $this->assertEquals(202, $imageInfo[0]); // Image width
+        $this->assertEquals(45, $imageInfo[1]); // Image height
+        $this->assertEquals('image/jpeg', $imageInfo['mime']);
+    }
+
+    public function test_jpg_barcode_generator_can_use_different_width_factor()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorJPG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128, 5);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(1000, strlen($result));
+        $this->assertEquals(505, $imageInfo[0]); // Image width
+        $this->assertEquals('image/jpeg', $imageInfo['mime']);
+    }
+
+    public function test_png_barcode_generator_can_generate_code_39_barcode()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_39, 1);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(100, strlen($result));
+        $this->assertEquals(224, $imageInfo[0]); // Image width
+        $this->assertEquals(30, $imageInfo[1]); // Image height
+        $this->assertEquals('image/png', $imageInfo['mime']);
+    }
+
+    public function test_png_barcode_generator_can_use_different_height()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128, 2, 45);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(100, strlen($result));
+        $this->assertEquals(202, $imageInfo[0]); // Image width
+        $this->assertEquals(45, $imageInfo[1]); // Image height
+        $this->assertEquals('image/png', $imageInfo['mime']);
+    }
+
+    public function test_png_barcode_generator_can_use_different_width_factor()
+    {
+        $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        $result = $generator->getBarcode('081231723897', $generator::TYPE_CODE_128, 5);
+
+        $imageInfo = getimagesizefromstring($result);
+
+        $this->assertGreaterThan(100, strlen($result));
+        $this->assertEquals(505, $imageInfo[0]); // Image width
+        $this->assertEquals('image/png', $imageInfo['mime']);
     }
 
     public function test_ean13_generator_throws_exception_if_invalid_chars_are_used()
