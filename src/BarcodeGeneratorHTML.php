fdd368a88946e3d01c6fd9397d56e082d6b8d64a
Casper Bakker <casper@picqer.com>
2020-01-03 09:54:08 +0100

    Unify generator interface names

--- src/BarcodeGeneratorHTML.php
+++ src/BarcodeGeneratorHTML.php
@@ -10,24 +10,23 @@ class BarcodeGeneratorHTML extends BarcodeGenerator
      * @param string $barcode code to print
      * @param string $type type of barcode
      * @param int $widthFactor Width of a single bar element in pixels.
-     * @param int $totalHeight Height of a single bar element in pixels.
-     * @param int|string $foregroundColor Foreground color for bar elements as '#333' or 'orange' for example (background is transparent).
+     * @param int $height Height of a single bar element in pixels.
+     * @param string $foregroundColor Foreground color for bar elements as '#333' or 'orange' for example (background is transparent).
      * @return string HTML code.
-     * @public
      */
-    public function getBarcode($barcode, $type, int $widthFactor = 2, int $totalHeight = 30, string $foregroundColor = 'black')
+    public function getBarcode($barcode, $type, int $widthFactor = 2, int $height = 30, string $foregroundColor = 'black')
     {
         $barcodeData = $this->getBarcodeData($barcode, $type);
 
-        $html = '<div style="font-size:0;position:relative;width:' . ($barcodeData['maxWidth'] * $widthFactor) . 'px;height:' . ($totalHeight) . 'px;">' . PHP_EOL;
+        $html = '<div style="font-size:0;position:relative;width:' . ($barcodeData['maxWidth'] * $widthFactor) . 'px;height:' . ($height) . 'px;">' . PHP_EOL;
 
         $positionHorizontal = 0;
         foreach ($barcodeData['bars'] as $bar) {
             $barWidth = round(($bar['width'] * $widthFactor), 3);
-            $barHeight = round(($bar['height'] * $totalHeight / $barcodeData['maxHeight']), 3);
+            $barHeight = round(($bar['height'] * $height / $barcodeData['maxHeight']), 3);
 
             if ($bar['drawBar']) {
-                $positionVertical = round(($bar['positionVertical'] * $totalHeight / $barcodeData['maxHeight']), 3);
+                $positionVertical = round(($bar['positionVertical'] * $height / $barcodeData['maxHeight']), 3);
                 // draw a vertical bar
                 $html .= '<div style="background-color:' . $foregroundColor . ';width:' . $barWidth . 'px;height:' . $barHeight . 'px;position:absolute;left:' . $positionHorizontal . 'px;top:' . $positionVertical . 'px;">&nbsp;</div>' . PHP_EOL;
             }
--- src/BarcodeGeneratorSVG.php
+++ src/BarcodeGeneratorSVG.php
@@ -10,12 +10,12 @@ class BarcodeGeneratorSVG extends BarcodeGenerator
      * @param $barcode (string) code to print
      * @param $type (const) type of barcode
      * @param $widthFactor (int) Minimum width of a single bar in user units.
-     * @param $totalHeight (int) Height of barcode in user units.
+     * @param $height (int) Height of barcode in user units.
      * @param $foregroundColor (string) Foreground color (in SVG format) for bar elements (background is transparent).
      * @return string SVG code.
      * @public
      */
-    public function getBarcode($barcode, $type, int $widthFactor = 2, int $totalHeight = 30, string $foregroundColor = 'black')
+    public function getBarcode($barcode, $type, int $widthFactor = 2, int $height = 30, string $foregroundColor = 'black')
     {
         $barcodeData = $this->getBarcodeData($barcode, $type);
 
@@ -26,16 +26,16 @@ class BarcodeGeneratorSVG extends BarcodeGenerator
 
         $svg = '<?xml version="1.0" standalone="no" ?>' . PHP_EOL;
         $svg .= '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' . PHP_EOL;
-        $svg .= '<svg width="' . $width . '" height="' . $totalHeight . '" viewBox="0 0 ' . $width . ' ' . $totalHeight . '" version="1.1" xmlns="http://www.w3.org/2000/svg">' . PHP_EOL;
+        $svg .= '<svg width="' . $width . '" height="' . $height . '" viewBox="0 0 ' . $width . ' ' . $height . '" version="1.1" xmlns="http://www.w3.org/2000/svg">' . PHP_EOL;
         $svg .= "\t" . '<desc>' . strtr($barcodeData['code'], $repstr) . '</desc>' . PHP_EOL;
         $svg .= "\t" . '<g id="bars" fill="' . $foregroundColor . '" stroke="none">' . PHP_EOL;
         // print bars
         $positionHorizontal = 0;
         foreach ($barcodeData['bars'] as $bar) {
             $barWidth = round(($bar['width'] * $widthFactor), 3);
-            $barHeight = round(($bar['height'] * $totalHeight / $barcodeData['maxHeight']), 3);
+            $barHeight = round(($bar['height'] * $height / $barcodeData['maxHeight']), 3);
             if ($bar['drawBar']) {
-                $positionVertical = round(($bar['positionVertical'] * $totalHeight / $barcodeData['maxHeight']), 3);
+                $positionVertical = round(($bar['positionVertical'] * $height / $barcodeData['maxHeight']), 3);
                 // draw a vertical bar
                 $svg .= "\t\t" . '<rect x="' . $positionHorizontal . '" y="' . $positionVertical . '" width="' . $barWidth . '" height="' . $barHeight . '" />' . PHP_EOL;
             }
