8279d3def5bd13261a7f697db5fa29048fb00875
Darren Stephens <illuminatusds@users.noreply.github.com>
2023-09-15 17:18:50 +0100

    updates/adjustments in response to review

--- src/Types/TypeTelepen.php
+++ src/Types/TypeTelepen.php
@@ -19,21 +19,21 @@ use Picqer\Barcode\Barcode;
 use Picqer\Barcode\BarcodeBar;
 use Picqer\Barcode\Exceptions\InvalidFormatException;
 
-define('TELEPEN_START_CHAR', '_');
-define('TELEPEN_STOP_CHAR',  'z');
-define('TELEPEN_ALPHA',   'alpha');
-define('TELEPEN_NUMERIC', 'numeric');
-
 class TypeTelepen implements TypeInterface
 {
+    private const TELEPEN_START_CHAR = '_';
+    private const TELEPEN_STOP_CHAR = 'z';
+    private const TELEPEN_ALPHA = 'alpha';
+    private const TELEPEN_NUMERIC = 'numeric';
+
     private $telepen_lookup_table;
     private $mode;
 
     public function __construct($m = 'alpha')
     {
-        $this->mode = TELEPEN_ALPHA;
+        $this->mode = self::TELEPEN_ALPHA;
         if (strtolower($m) == 'numeric') {
-            $this->mode = TELEPEN_NUMERIC;
+            $this->mode = self::TELEPEN_NUMERIC;
         }
         $this->createTelepenConversionTable();
     }
@@ -60,7 +60,7 @@ class TypeTelepen implements TypeInterface
     protected function encode($code) : string
     {
         $result = null;
-        if ($this->mode == TELEPEN_ALPHA) {
+        if ($this->mode == self::TELEPEN_ALPHA) {
             $result = $this->encodeAlpha($code);
         } else {
             $result = $this->encodeNumeric($code);
@@ -71,8 +71,10 @@ class TypeTelepen implements TypeInterface
 
     protected function encodeAlpha($code) : string
     {
-
-        if (!preg_match('/[ -~]+/', $code)) { // everything from ASCII32-ASCII127
+        // We aren't interested in the non-printable parts of the ASCII set
+        if (
+            !preg_match('/[ -~]+/', $code)
+        ) { // everything from ASCII32-ASCII127
             throw new InvalidFormatException("Invalid characters in data");
         }
 
@@ -84,7 +86,7 @@ class TypeTelepen implements TypeInterface
          */
 
         // begin with start char
-        $dest = $this->telepen_lookup_table[ord(TELEPEN_START_CHAR)];
+        $dest = $this->telepen_lookup_table[ord(self::TELEPEN_START_CHAR)];
 
         for ($i = 0; $i < strlen($code); $i++) {
             //$ascii_code = ord(substr($code, $i, 1));
@@ -100,7 +102,7 @@ class TypeTelepen implements TypeInterface
         }
 
         $dest .= $this->telepen_lookup_table[ord($check_digit)];
-        $dest .= $this->telepen_lookup_table[ord(TELEPEN_STOP_CHAR)]; // Stop
+        $dest .= $this->telepen_lookup_table[ord(self::TELEPEN_STOP_CHAR)]; // Stop
 
         return $dest;
     }
@@ -120,7 +122,7 @@ class TypeTelepen implements TypeInterface
         }
 
         $count = 0;
-        $dest = $this->telepen_lookup_table[ord(TELEPEN_START_CHAR)]; // begin with the start character _
+        $dest = $this->telepen_lookup_table[ord(self::TELEPEN_START_CHAR)]; // begin with the start character _
         
         for ($i = 0; $i < strlen($code); $i += 2) {
             $c1 = $code[$i];
@@ -145,42 +147,54 @@ class TypeTelepen implements TypeInterface
         }
 
         $dest .= $this->telepen_lookup_table[$check_digit];
-        $dest .= $this->telepen_lookup_table[ord(TELEPEN_STOP_CHAR)]; // Stop
+        $dest .= $this->telepen_lookup_table[ord(self::TELEPEN_STOP_CHAR)]; // Stop
 
         return $dest;
     }
 
+    /**
+     * The table provides a representation of barcode patterns 
+     * for each character in the ASCII set. from ASCII0-127
+     * Each barcode starts with "_" - ASCII95 0x5F, 
+     * and ends with "z" - ASCII122 0xFA.
+     * More information about Telepen symbology is available from
+     * https://v4l237.n3cdn1.secureserver.net/wp-content/uploads/2022/05/Barcode-Symbology-information-and-History.pdf
+     */
     private function createTelepenConversionTable()
     {
         $this->telepen_lookup_table = [
-            "1111111111111111", "1131313111", "33313111", "1111313131",
-            "3111313111", "11333131", "13133131", "111111313111", "31333111",
-            "1131113131", "33113131", "1111333111", "3111113131", "1113133111",
-            "1311133111", "111111113131", "3131113111", "11313331", "333331",
-            "111131113111", "31113331", "1133113111", "1313113111", "1111113331",
-            "31131331", "113111113111", "3311113111", "1111131331", "311111113111",
-            "1113111331", "1311111331", "11111111113111", "31313311", "1131311131",
-            "33311131", "1111313311", "3111311131", "11333311", "13133311",
-            "111111311131", "31331131", "1131113311", "33113311", "1111331131",
-            "3111113311", "1113131131", "1311131131", "111111113311", "3131111131",
-            "1131131311", "33131311", "111131111131", "3111131311", "1133111131",
-            "1313111131", "111111131311", "3113111311", "113111111131",
-            "3311111131", "111113111311", "311111111131", "111311111311",
-            "131111111311", "11111111111131", "3131311111", "11313133", "333133",
-            "111131311111", "31113133", "1133311111", "1313311111", "1111113133",
-            "313333", "113111311111", "3311311111", "11113333", "311111311111",
-            "11131333", "13111333", "11111111311111", "31311133", "1131331111",
-            "33331111", "1111311133", "3111331111", "11331133", "13131133",
-            "111111331111", "3113131111", "1131111133", "33111133", "111113131111",
-            "3111111133", "111311131111", "131111131111", "111111111133",
-            "31311313", "113131111111", "3331111111", "1111311313", "311131111111",
-            "11331313", "13131313", "11111131111111", "3133111111", "1131111313",
-            "33111313", "111133111111", "3111111313", "111313111111",
-            "131113111111", "111111111313", "313111111111", "1131131113",
-            "33131113", "11113111111111", "3111131113", "113311111111",
-            "131311111111", "111111131113", "3113111113", "11311111111111",
-            "331111111111", "111113111113", "31111111111111", "111311111113",
-            "131111111113"
+            "1111111111111111", "1131313111", "33313111", "1111313131", 
+            "3111313111", "11333131", "13133131", "111111313111",
+            "31333111", "1131113131", "33113131", "1111333111", 
+            "3111113131", "1113133111", "1311133111", "111111113131", 
+            "3131113111", "11313331", "333331", "111131113111", 
+            "31113331", "1133113111", "1313113111", "1111113331", 
+            "31131331", "113111113111", "3311113111", "1111131331", 
+            "311111113111", "1113111331", "1311111331", "11111111113111", 
+            "31313311", "1131311131", "33311131", "1111313311", 
+            "3111311131", "11333311", "13133311", "111111311131", 
+            "31331131", "1131113311", "33113311", "1111331131", 
+            "3111113311", "1113131131", "1311131131", "111111113311", 
+            "3131111131", "1131131311", "33131311", "111131111131", 
+            "3111131311", "1133111131", "1313111131", "111111131311", 
+            "3113111311", "113111111131", "3311111131", "111113111311", 
+            "311111111131", "111311111311", "131111111311", "11111111111131", 
+            "3131311111", "11313133", "333133", "111131311111", 
+            "31113133", "1133311111", "1313311111", "1111113133", 
+            "313333", "113111311111", "3311311111", "11113333", 
+            "311111311111", "11131333", "13111333", "11111111311111", 
+            "31311133", "1131331111", "33331111", "1111311133", 
+            "3111331111", "11331133", "13131133", "111111331111", 
+            "3113131111", "1131111133", "33111133", "111113131111", 
+            "3111111133", "111311131111", "131111131111", "111111111133", 
+            "31311313", "113131111111", "3331111111", "1111311313", 
+            "311131111111", "11331313", "13131313", "11111131111111", 
+            "3133111111", "1131111313", "33111313", "111133111111", 
+            "3111111313", "111313111111", "131113111111", "111111111313", 
+            "313111111111", "1131131113", "33131113", "11113111111111", 
+            "3111131113", "113311111111", "131311111111", "111111131113", 
+            "3113111113", "11311111111111", "331111111111", "111113111113", 
+            "31111111111111", "111311111113", "131111111113"
         ];
     }
 }
