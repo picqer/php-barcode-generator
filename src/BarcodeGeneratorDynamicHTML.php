ec4502fad452dd7581989df982f128a26e6451e1
Casper Bakker <casper@picqer.com>
2024-03-31 20:34:00 +0200

    HTML renderers

--- src/BarcodeGeneratorDynamicHTML.php
+++ src/BarcodeGeneratorDynamicHTML.php
@@ -4,8 +4,6 @@ namespace Picqer\Barcode;
 
 class BarcodeGeneratorDynamicHTML extends BarcodeGenerator
 {
-    private const WIDTH_PRECISION = 6;
-
     /**
      * Return an HTML representation of barcode.
      * This 'dynamic' version uses percentage based widths and heights, resulting in a vector-y qualitative result.
@@ -19,26 +17,9 @@ class BarcodeGeneratorDynamicHTML extends BarcodeGenerator
     {
         $barcodeData = $this->getBarcodeData($barcode, $type);
 
-        $html = '<div style="font-size:0;position:relative;width:100%;height:100%">' . PHP_EOL;
+        $renderer = new \Picqer\Barcode\Renderers\DynamicHtmlRenderer();
+        $renderer->setForegroundColor($foregroundColor);
 
-        $positionHorizontal = 0;
-        /** @var BarcodeBar $bar */
-        foreach ($barcodeData->getBars() as $bar) {
-            $barWidth = $bar->getWidth() / $barcodeData->getWidth() * 100;
-            $barHeight = round(($bar->getHeight() / $barcodeData->getHeight() * 100), 3);
-
-            if ($bar->isBar() && $barWidth > 0) {
-                $positionVertical = round(($bar->getPositionVertical() / $barcodeData->getHeight() * 100), 3);
-
-                // draw a vertical bar
-                $html .= '<div style="background-color:' . $foregroundColor . ';width:' . round($barWidth, self::WIDTH_PRECISION) . '%;height:' . $barHeight . '%;position:absolute;left:' . round($positionHorizontal, self::WIDTH_PRECISION) . '%;top:' . $positionVertical . (($positionVertical > 0) ? '%' : '') . '">&nbsp;</div>' . PHP_EOL;
-            }
-
-            $positionHorizontal += $barWidth;
-        }
-
-        $html .= '</div>' . PHP_EOL;
-
-        return $html;
+        return $renderer->render($barcodeData);
     }
 }
