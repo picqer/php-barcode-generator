a30fa461737375c72bb1e32d288ca22805310b3b
Dan Waddell <dan.waddell@gear4music.com>
2024-11-03 22:28:33 +0000

    build: Remove JFrog repos

--- composer-package-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fsSL --user "${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}" \
-"${ARTIFACTORY_URL}scripts-local/composer-package-deploy/composer-package-deploy.sh" \
-| bash -s -- "`echo $TRAVIS_REPO_SLUG | awk 'BEGIN { FS = "/" } ; { print $2 }'`" "${TRAVIS_TAG}"
