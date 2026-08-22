37883ddcda9654015064532a756037bd6a40c911
Casper Bakker <casper@picqer.com>
2026-08-22 11:25:17 +0200

    phpstan to level 8

--- src/Helpers/StringHelpers.php
+++ src/Helpers/StringHelpers.php
@@ -6,7 +6,12 @@ class StringHelpers
 {
     public static function getSafeFilenameFrom(string $className): string {
         $lastPart = substr($className, strrpos($className, '\\') + 1);
+        $safeFilename = preg_replace('/[^a-zA-Z0-9_ \-+]/s', '-', $lastPart);
 
-        return preg_replace('/[^a-zA-Z0-9_ \-+]/s', '-', $lastPart);
+        if ($safeFilename === null) {
+            throw new \RuntimeException('Could not sanitize filename.');
+        }
+
+        return $safeFilename;
     }
 }
